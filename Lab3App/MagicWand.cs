adca0239cd4fa9752e4e9e304a72840f355a24e2
maheepw <mwadhwa5@uwo.ca>
2024-01-31 20:50:36 -0500

    first commit

---
+++ Lab3App/MagicWand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3App
+{
+    internal class MagicWand : Tool
+    {
+        public MagicWand(String a)
+        {
+            Description = a;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("MagicWand " + Description + " is displayed");
+        }
+        public override void DoAction()
+        {
+            Console.WriteLine("MagicWand is Used");
+        }
+    }
+}
